70bafef413184efde69f7778293416a85f7bd600
Rishabh <76615067+RISHABH-GUPTA-RG@users.noreply.github.com>
2025-04-23 20:48:20 +0530

    converted typescript to javascript

---
+++ public/src/components/types/songTypes.js
@@ -0,0 +1,13 @@
+// This file defines the structure of a SongType object in JavaScript.
+
+const SongType = {
+  _id: undefined,
+  name: undefined,
+  album: undefined,
+  rating: undefined,
+  singers: undefined,
+  genre: undefined,
+  url: undefined,
+};
+
+export default SongType;
